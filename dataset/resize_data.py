7830009a8c4a571feecf4f2c4995044100bf5813
Arcturion <dimascours@gmail.com>
2022-10-10 19:59:54 +0800

    Update resize_data.py
    Selesai resize+mean bulanan

--- dataset/resize_data.py
+++ dataset/resize_data.py
@@ -1,28 +1,20 @@
 import xarray as xr
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 
 ds = xr.open_dataset("u_v_data_mau_diresize.nc")
 target = xr.open_dataset("eaeaea.nc")
 
-# took the code from https://github.com/kbongi/models/blob/c6ecfa7facbd836965b37e5715b2f9ae3ee4d4e9/frequently_used_functions.py
-def monthly_mean(dataset, start_date, end_date):
-    
-    #variable_monthly = dataset.groupby('time.month')
-
-    monthly_mean = dataset.sel(time = slice(f'{start_date}', f'{end_date}')).groupby('time.month').mean(dim = 'time')
-
-    return monthly_mean
-
 year = ['2002','2003']
-for y in year:
-    ds_mean = monthly_mean(ds, y, y)
-    for m in range(1, 13):        
-        new_lon = np.linspace(ds_sel.longitude[0], ds_sel.longitude[-1], target.dims["lon"])
-        new_lat = np.linspace(ds_sel.latitude[0], ds_sel.latitude[-1], target.dims["lat"])
-        
-        dsi = ds_sel.interp(latitude=new_lat, longitude=new_lon)
-        dsi.to_netcdf(str(y)+"{:02d}".format(m)+".nc")
 
-dsi.v10.plot()
+year_month_idx = pd.MultiIndex.from_arrays([np.array(ds['time.year']), np.array(ds['time.month'])])
+ds.coords['year_month'] = ('time', year_month_idx)
+ds_mean = ds.groupby('year_month').mean()
 
+new_lon = np.linspace(ds_mean.longitude[0], ds_mean.longitude[-1], target.dims["lon"])
+new_lat = np.linspace(ds_mean.latitude[0], ds_mean.latitude[-1], target.dims["lat"])
+
+dsi = ds_mean.interp(latitude=new_lat, longitude=new_lon)
+
+dsi.sel(year_month=ds_mean.year_month[238]).u10.plot()
