422af5b54f630d44ff02fae5b00f127d1b34a437
Arcturion <dimascours@gmail.com>
2022-10-10 17:47:13 +0800

    Update resize_data.py
    more robust monthly mean

--- dataset/resize_data.py
+++ dataset/resize_data.py
@@ -3,15 +3,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 ds = xr.open_dataset("u_v_data_mau_diresize.nc")
-ds = ds.sel(time='2002-01')
 target = xr.open_dataset("eaeaea.nc")
 
-for y in range(2002, 2003):
-    for m in range(1, 2):
-        ds_sel = ds.sel(time=np.isin(ds.time.dt.month, m))
-        ds_sel = ds.sel(time=np.isin(ds.time.dt.year, y))
-        ds_sel = ds_sel.mean(dim='time')
-        
+# took the code from https://github.com/kbongi/models/blob/c6ecfa7facbd836965b37e5715b2f9ae3ee4d4e9/frequently_used_functions.py
+def monthly_mean(dataset, start_date, end_date):
+    
+    #variable_monthly = dataset.groupby('time.month')
+
+    monthly_mean = dataset.sel(time = slice(f'{start_date}', f'{end_date}')).groupby('time.month').mean(dim = 'time')
+
+    return monthly_mean
+
+year = ['2002','2003']
+for y in year:
+    ds_mean = monthly_mean(ds, y, y)
+    for m in range(1, 13):        
         new_lon = np.linspace(ds_sel.longitude[0], ds_sel.longitude[-1], target.dims["lon"])
         new_lat = np.linspace(ds_sel.latitude[0], ds_sel.latitude[-1], target.dims["lat"])
         
