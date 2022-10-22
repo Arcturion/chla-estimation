b8896fcd7715d9daab984940dcf8ea251f5465df
Arcturion <dimascours@gmail.com>
2022-10-22 21:55:59 +0800

    Update nyatuin_chla.py
    kesalahan freq date from M to MS

--- dataset/nyatuin_chla.py
+++ dataset/nyatuin_chla.py
@@ -14,8 +14,8 @@ full_path2 = os.path.join(path, path_data[0])
 data_stack = xr.open_dataset(full_path2)
 data_input = xr.open_dataset(full_path1)
 
-times = pd.date_range("2002/01/01","2002/02/02",freq='M', inclusive="left")
-times2 = pd.date_range("2002/02/01","2002/03/02",freq='M', inclusive="left")
+times = pd.date_range("2002/01/01","2002/02/02",freq='MS', inclusive="left")
+times2 = pd.date_range("2002/02/01","2002/03/02",freq='MS', inclusive="left")
 
 time_da = xr.DataArray(times, [('time', times)])
 time_da2 = xr.DataArray(times2, [('time', times)])
