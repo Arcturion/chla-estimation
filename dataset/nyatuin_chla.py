39c4777f67b675b7e6c7ab903645f18897f295d9
Arcturion <dimascours@gmail.com>
2022-10-22 21:49:40 +0800

    Update nyatuin_chla.py
    sukses test dua file doang
    Next, bikin looping untuk semua data dalam satu folder

--- dataset/nyatuin_chla.py
+++ dataset/nyatuin_chla.py
@@ -14,7 +14,15 @@ full_path2 = os.path.join(path, path_data[0])
 data_stack = xr.open_dataset(full_path2)
 data_input = xr.open_dataset(full_path1)
 
-flist1 = [1,2,3]
-data_gabungan = xr.concat([data_stack, data_input],dim=['time','lon','lat'])
+times = pd.date_range("2002/01/01","2002/02/02",freq='M', inclusive="left")
+times2 = pd.date_range("2002/02/01","2002/03/02",freq='M', inclusive="left")
+
+time_da = xr.DataArray(times, [('time', times)])
+time_da2 = xr.DataArray(times2, [('time', times)])
+
+data_input = data_input.expand_dims(time=time_da)
+data_stack = data_stack.expand_dims(time=time_da2)
+
+data_gabungan = xr.concat([data_input, data_stack], dim='time')
 
 data_gabungan
